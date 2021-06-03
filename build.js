c1bb62db0158100912bb885cdebc735cff849c94
Mert Doğu <mertdoguonder@gmail.com>
2021-06-03 09:56:59 +0300

    Add files via upload

---
+++ build.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:hi@anmolsingh.me");
+                    console.log("\nYeyy! I'll wait your mail :)\n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Bye. See ya later!\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Anmol Pratap Singh"),
+    handle: chalk.white("@rexulec"),
+    work: `${chalk.white("Freelance	")}
+
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("misteranmol"),
+    github: chalk.gray("https://github.com/") + chalk.green("anmol098"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("anmol098"),
+    web: chalk.cyan("http://rexulec.com"),
+    npx: chalk.red("npx") + " " + chalk.white("rexulec"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities,"
+        )}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
